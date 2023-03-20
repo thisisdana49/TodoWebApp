ec1a372971e82b3a03c17c983dd997c754c9c164
thisisdana49 <thisisdana49@gmail.com>
2023-03-20 16:04:57 +0900

    3/20 Controller Layer

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -16,5 +19,33 @@ public class TestController {
     @GetMapping("/{id}")
     public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
         return "Hello World! ID: " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerWithRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! ID: " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World! ID: " + testRequestBodyDTO.getId() + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello world! I'm Dana's ResponseDTO!");
+        list.add("Hello?");
+        list.add("Are you there?");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello world! I'm Dana's ResponseDTO! You got 200");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
     }
 }
